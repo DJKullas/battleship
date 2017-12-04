f89e83621ddaaa1a269f66ad5fe4b68a8ea06966
DJ Kullas <dkoolj5@gmail.com>
2017-12-04 18:27:12 -0500

    distillery

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :battleship, BattleshipWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
