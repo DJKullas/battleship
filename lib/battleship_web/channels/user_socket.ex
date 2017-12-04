5bab2f80f72536304f1183455c17d3594aeb9fdf
DJ Kullas <dkoolj5@gmail.com>
2017-12-04 17:22:58 -0500

    Fixed

--- lib/battleship_web/channels/user_socket.ex
+++ lib/battleship_web/channels/user_socket.ex
@@ -6,8 +6,7 @@ defmodule BattleshipWeb.UserSocket do
    channel "player:*", BattleshipWeb.PlayerChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket,
-    timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
